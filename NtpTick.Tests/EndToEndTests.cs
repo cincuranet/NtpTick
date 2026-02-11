f3ca9834ec84ad9db6c92fc980fede238a7ebaa8
Jiri Cincura <jiri@cincura.net>
2026-02-11 19:02:23 +0100

    Improve assert.

--- NtpTick.Tests/EndToEndTests.cs
+++ NtpTick.Tests/EndToEndTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace NtpTick.Tests;
 
@@ -18,16 +17,19 @@ public class EndToEndTests
     public async Task Send_ReturnsSensiblePacket()
     {
         var response = await _client.Send(new NtpPacket());
-        Assert.That(response.Version, Is.EqualTo(NtpVersion.Version4));
-        Assert.That(response.Mode, Is.EqualTo(NtpMode.Server));
-        Assert.That(response.Stratum, Is.GreaterThan(0).And.LessThanOrEqualTo(2));
-        Assert.That(response.Poll, Is.GreaterThan(-30));
-        Assert.That(response.Precision, Is.GreaterThan(-30));
-        Assert.That(response.RootDelay, Is.GreaterThan(0));
-        Assert.That(response.RootDispersion, Is.GreaterThan(0));
-        Assert.That(response.ReferenceTimestamp, Is.Not.Default);
-        Assert.That(response.ReceiveTimestamp, Is.Not.Default);
-        Assert.That(response.TransmitTimestamp, Is.Not.Default);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(response.Version, Is.EqualTo(NtpVersion.Version4));
+            Assert.That(response.Mode, Is.EqualTo(NtpMode.Server));
+            Assert.That(response.Stratum, Is.GreaterThan(0).And.LessThanOrEqualTo(2));
+            Assert.That(response.Poll, Is.GreaterThan(-30));
+            Assert.That(response.Precision, Is.GreaterThan(-30));
+            Assert.That(response.RootDelay, Is.GreaterThan(0));
+            Assert.That(response.RootDispersion, Is.GreaterThan(0));
+            Assert.That(response.ReferenceTimestamp, Is.Not.Default);
+            Assert.That(response.ReceiveTimestamp, Is.Not.Default);
+            Assert.That(response.TransmitTimestamp, Is.Not.Default);
+        }
     }
 
     [Test]
